b5e752ff9062d47a0090f5cafb5d142705759758
Ignitoss <kocherchristopher@outlook.com>
2022-08-02 05:50:57 -0500

    8-2-22

---
+++ script.js
@@ -0,0 +1,83 @@
+const carousel = function () {
+  const slides = document.querySelectorAll(".carousel");
+  const btnLeft = document.querySelector(".btn-left");
+  const btnRight = document.querySelector(".btn-right");
+  const dotcontainer = document.querySelector(".dots-dot");
+
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotcontainer.insertAdjacentHTML(
+        "beforeend",
+        `<button class="dots-dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll(".dots-dot")
+      .forEach((dot) => dot.classList.remove("dots-dot--active"));
+
+    document
+      .querySelector(`dots-dot[data-slide="${slide}"]`)
+      .classList.add("dots-dot--active");
+  };
+
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
+
+  // Next slide
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  // Previous slide
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const init = function () {
+    createDots();
+    activateDot(0);
+    goToSlide(0);
+  };
+  init();
+
+  // Event Handlers
+  btnRight.addEventListener("click", nextSlide);
+  btnLeft.addEventListener("click", prevSlide);
+
+  //   Allowing scrolling via keyboard arrows
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") prevSlide();
+    if (e.key === "ArrowRight") nextSlide();
+  });
+
+  dotcontainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dots-dot")) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activateDot(slide);
+    }
+  });
+};
+carousel();
