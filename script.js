77224bbd28c22379f049c7b0fa8ec53dd1c8e68c
Christopher Kocher <kocherchristopher@outlook.com>
2022-08-30 21:53:44 -0500

    8-30-22

--- script.js
+++ script.js
@@ -10,10 +10,14 @@ copyrightYear();
 ////////////////////////////////////////////////////////////////////////////////////////
 // mobile nav
 const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".header");
+const headerEl = document.querySelector(".nav-links-mobile");
+const mobileNavEl = document.querySelectorAll(".icon-mobile-nav");
 
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
+  mobileNavEl.forEach((el) => {
+    el.classList.toggle("nav-open");
+  });
 });
 ("nav-open");
 
